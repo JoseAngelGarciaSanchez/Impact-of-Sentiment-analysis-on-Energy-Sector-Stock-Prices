11fbb0f173b84ac9c30fdf9c982f9930378f947d
JAGS <78938133+JoseAngelGarciaSanchez@users.noreply.github.com>
2024-04-13 18:21:47 +0200

    feature/main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import sys
+
 from preprocessing.preprocessing import PreprocessorPipeline
 
 
-PreprocessorPipeline()
+path = sys.argv[1]
+output_path = "./../data_cleaned/" + path.split("/")[-1]
+preprocessing = PreprocessorPipeline(path=path, output_path=output_path)
+preprocessing.launch()
