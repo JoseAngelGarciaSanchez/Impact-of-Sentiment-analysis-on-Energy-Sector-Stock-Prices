ef1d2400d1e5a47abaee57188565a8c9e275fd93
JAGS <78938133+JoseAngelGarciaSanchez@users.noreply.github.com>
2024-04-13 20:27:48 +0200

    feature/preprocessing from pyspark to python

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,188 +1,104 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from langdetect import detect
-import nltk
-import sys
 import re
+import sys
 
-
-nltk.download("punkt")
-nltk.download("perluniprops")
-
+import pandas as pd
 
 class PreprocessorPipeline:
-    """
-    preprocess class for cleaning webscrapped tweets
-    """
-    def __init__(self, path, output_path):
-        self.path = path
-        self.output_path = output_path
-
-    def import_df(self):
-        """Importing the webscrapped df to preprocess..."""
-
-        print("---Importing the tweets...")
-
-        spark = SparkSession.builder.appName("CSVtoTable").getOrCreate()
-
-        df = (
-            spark.read.format("csv")
-            .option("mode", "DROPMALFORMED")
-            .options(header=True, inferSchema=True, sep=",", multiLine=True)
-            .load(self.path)
-            .cache()
-        )  # Keep the dataframe in memory for faster processing
-
-        return df
-
-    def cast_columns(self, df):
-        """This function casts good columns types : ints and dates"""
-        print("---Changing the type of columns...")
-
-        # Casting integers
-        df = df.select(
-            *[
-                col(c).cast("integer").alias(c)
-                if c in ["ReplyCount", "RetweetCount", "LikeCount"]
-                else c
-                for c in df.columns
-            ]
-        )
-        # Casting dates
-        df = df.withColumn("PostDate", to_date(df["PostDate"]))
-
-        return df
-
-    def cleaning_tweets(self, df):
-        """In this function, we're cleaning the text of the tweets. We're trimming all the special characters, the links,
-        and keeping only the text part.
+    def __init__(self, verbose: bool = False) -> None:
+        self.verbose = verbose
+    
+    def _dealing_with_na(self, df):
         """
-        print("---Cleaning the tweets with regex...")
-
-        # Lowering all characters
-        df = df.withColumn("TweetText", lower(col("TweetText")))
-
-        # Deleting special characters (\n)
-        df = df.withColumn("TweetText", trim(regexp_replace("TweetText", "[\n]+", " ")))
-
-        # Deleting URLs
-        df = df.withColumn(
-            "TweetText", trim(regexp_replace("TweetText", "https?://[^ ]+", ""))
-        )
-
-        # Deleting hashtags and mentions
-        df = df.withColumn("TweetText", trim(regexp_replace("TweetText", "#[^ ]+", "")))
-        df = df.withColumn("TweetText", trim(regexp_replace("TweetText", "@[^ ]+", "")))
-
-        # Deleting any character that is not an uppercase or lowercase letter, a digit, or a space
-        df = df.withColumn(
-            "TweetText", trim(regexp_replace("TweetText", "[^A-Za-z0-9 ]", ""))
-        )
-
-        # Keeping only the text part of the tweets
-        df = df.withColumn(
-            "TweetText",
-            regexp_extract("TweetText", "(?<=2017|2018|2019|2020|2021|2022)(.*)", 0),
-        )
-
-        # Spliting by word boundaries
-        def remove_non_word(text):
-            pattern = re.compile(r"\W+")
-            return pattern.sub(" ", text)
-
-        remove_non_word_udf = udf(remove_non_word)
-        df = df.withColumn("TweetText", remove_non_word_udf("TweetText"))
-
-        # Repeating words like hurrrryyyyyy
-        rpt_regex = re.compile(r"(.)\1{1,}", re.IGNORECASE)
-
-        def rpt_repl(match):
-            return match.group(1) + match.group(1)
-
-        rpt_udf = udf(lambda x: re.sub(rpt_regex, rpt_repl, x))
-        df = df.withColumn("TweetText", rpt_udf("TweetText"))
-
-        # Dropping duplicates
-        df = df.dropDuplicates(["TweetText"])
-
-        return df
-
-    def dealing_with_na(self, df):
-        """Here, we're dealing with NA values. For the int columns, we're filling the NAs with 0
+        Here, we're dealing with NA values. 
+        For the int columns, we're filling the NAs with 0
         and we're dropping the empty tweets.
         """
+        if self.verbose:
+            print("---Dealing with na values...")
 
-        print("---Dealing with na values...")
         # Filling 0 for int columns
-        df = df.na.fill(0, subset=["ReplyCount", "RetweetCount", "LikeCount"])
+        df.loc[:, ['ReplyCount', 'RetweetCount', 'LikeCount']] = df[['ReplyCount', 'RetweetCount', 'LikeCount']].fillna(0)
 
         # Dropping empty tweets
         df = df.dropna(subset=["TweetText"])
 
         return df
 
-    def loosing_handle(self, df):
+    def _cast_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        To do: format counts to int
+        """
+        if self.verbose:
+            print('---Changing the type of columns...')
+
+        df['PostDate'] = pd.to_datetime(df['PostDate'])
+
+        return df
+
+    def _cleaning_tweets(self, df, column):
+        """
+        In this function, we're cleaning the text of the tweets. 
+        We're trimming all the special characters, the links,
+        and keeping only the text part.
+        """
+        if self.verbose:
+            print(f'---Cleaning dataframe column: {column}')
+
+        # Lowering text
+        df[column] = df[column].str.lower()
+
+        # Deleting special characters (\n)
+        df[column] = df[column].str.replace(r"[\n]+", " ", regex=True)
+        
+        # Deleting any character before the ·
+        df[column] = df[column].apply(lambda tweet: tweet[tweet.find('·') + 1:] if '·' in tweet else tweet)
+
+        # Deleting URLs
+        df[column] = df[column].str.replace(r"https?://\S+", "", regex=True)
+
+        # Deleting any character that is not an uppercase or lowercase letter, a digit, or a space
+        df[column] = df[column].str.replace(r"[^A-Za-z0-9 ]", " ", regex=True)
+
+        # # Keeping only the text part of the tweets (if there is a need to extract after certain years)
+        # df[column] = df[column].str.extract(r"(?<=2017|2018|2019|2020|2021|2022|2023|2024)(.*)")[0]
+
+        # Splitting by word boundaries and replacing non-word characters
+        df[column] = df[column].str.replace(r"\W+", " ", regex=True).str.strip()
+
+        # Repeating words like hurrrryyyyyy
+        def rpt_repl(match):
+            return match.group(1) + match.group(1)
+        df[column] = df[column].apply(lambda x: re.sub(r"(.)\1{1,}", rpt_repl, x) if pd.notna(x) else x)
+
+        # Dropping duplicates
+        df = df.drop_duplicates(subset=[column])
+
+        return df
+    
+    def _loosing_handle(self, df):
         """This function looses the @ for the handles."""
+        
+        if self.verbose:
+            print("---Cleaning the handles...")
 
-        print("---Cleaning the handles...")
-
-        df = df.withColumn("Handle", trim(regexp_replace("Handle", "@", "")))
-
-        return df
-
-    def filtering_english(self, df):
-        """This function detects the langage of each tweets and filters
-        only those in english."""
-
-        print("---Choosing only the english tweets...")
-
-        df_pd = df.toPandas()
-
-        def detect_en(text):
-            try:
-                return detect(text) == "en"
-            except:
-                return False
-
-        df_pd = df_pd[df_pd["TweetText"].apply(detect_en)]
-
-        spark = SparkSession.builder.appName("CSVtoTable").getOrCreate()
-        df = spark.createDataFrame(df_pd)
+        df.loc[:, 'Handle'] = df['Handle'].str.replace('@', '').str.strip()
 
         return df
+    
+    def process(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = self._cast_columns(df)
+        df = self._cleaning_tweets(df, "TweetText")
+        df = self._loosing_handle(df)
+        df = self._dealing_with_na(df)
 
-    def creating_csv(self, df):
-        """Creating and saving the cleaned csv"""
+        if self.verbose:
+            print("Here is the result :) ")
+            print(df.head(3))
 
-        print("---Creating the cleaned csv!")
-
-        df.coalesce(1).write.format("csv").mode("overwrite").option(
-            "header", "true"
-        ).save(self.output_path)
         return df
+    
 
-    def launch(self) -> None:
-        df = preprocessing.import_df()
-        df.show()
-        df = preprocessing.cast_columns(df)
-        df.show()
-        df = preprocessing.cleaning_tweets(df)
-        df.show()
-        df = preprocessing.loosing_handle(df)
-        df.show()
-        df = preprocessing.filtering_english(df)
-        df.show()
-        df = preprocessing.dealing_with_na(df)
-        df.show()
-        preprocessing.creating_csv(df)
-        print("Here is the result :) ")
-        df.show()
-
-
-if __name__ == "__main__":
-
-    path = sys.argv[1]
-    output_path = "./../data_cleaned/" + path.split("/")[-1]
-    preprocessing = PreprocessorPipeline(path=path, output_path=output_path)
-    preprocessing.launch()
+if __name__ == '__main__':
+    df = pd.read_csv('./../data/new_webscrapping/webscraped_bp_plc.csv')
+    pp = PreprocessorPipeline(verbose=True)
+    cleaned_df = pp.process(df)
