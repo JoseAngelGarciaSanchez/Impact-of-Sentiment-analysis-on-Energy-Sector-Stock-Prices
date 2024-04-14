829cb27c34b72aa02b754f881d851b3f89e34ffb
JAGS <78938133+JoseAngelGarciaSanchez@users.noreply.github.com>
2024-04-14 20:23:29 +0200

    running model

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ preprocessor = PreprocessorPipeline()
 sentimental_model = SentimentalAnalysisModel()
 
 
-for dir, _, files in os.walk('./data/new_webscrapping/'):
+for dir, _, files in os.walk("./data/new_webscrapping/"):
     for file in files:
-        df = pd.read_csv(dir+file)
+        df = pd.read_csv(dir + file)
         df_processed = preprocessor.process(df)
         df_predicted = sentimental_model.predict(df_processed)
 
         # Saving transformations
-        df_processed.to_pickle(f'./data/new_webscrapping_clean/{file.split(".csv")[0]}.pkl')
-        df_predicted.to_csv(f'./data/new_webscrapping_predicted/{file.split(".csv")[0]}.pkl', index=False)
+        df_processed.to_pickle(
+            f'./data/new_webscrapping_clean/{file.split(".csv")[0]}.pkl'
+        )
+        df_predicted.to_csv(
+            f'./data/new_webscrapping_predicted/{file.split(".csv")[0]}.csv',
+            index=False,
+        )
 
     break
